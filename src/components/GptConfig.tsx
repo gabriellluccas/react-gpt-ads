65208f4be2fc95cd200b22fa86a92c1f69bbfaa8
Gabriell Lucas <gabriellluccas@gmail.com>
2021-03-30 13:36:41 -0300

    :bug: Add lazyload and events in the googletag.cmd queue
    Add googletag.cmd in configs() to push lazyload and events in the queue to fix a bug

--- src/components/GptConfig.tsx
+++ src/components/GptConfig.tsx
@@ -6,17 +6,20 @@ export const GptConfig: React.FC<GptConfigType> = ({
     let googletag: any
     
     const configs = () => {
+      googletag.cmd.push(() => {
+        console.log('pubads', googletag.pubads())
         if(enableLazyLoad) googletag.pubads().enableLazyLoad(enableLazyLoad)
         if(eventSlotRequested) googletag.pubads().addEventListener('slotRequested', eventSlotRequested)
         if(eventSlotOnload) googletag.pubads().addEventListener('slotOnload', eventSlotOnload)
+      })
     }
 
     useEffect(() => {
-        if(window.googletag && !googletag) {
+        if(window.googletag) {
           googletag = window.googletag
-          configs()
+          if(googletag.apiReady) configs()
         }
-      }, []) 
+      }, [])
 
     return null
 }
